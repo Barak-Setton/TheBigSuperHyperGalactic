e2fba94005c85af468d1df06e84927d7c0c62bcb
ResidentZealoT <resident@hotmail.co.za>
2016-09-06 04:21:24 +0200

    Added Basic Menu System
    Created a menu that allows for solo or networked play and a framework
    for selecting tracks and cars. Includes placeholder Game scenes.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Menu : MonoBehaviour 
+{
+	// Canvas objects
+	public Canvas ModeCanvas;
+	public Canvas SelectionCanvas;
+	public Image SplashScreen;
+
+	// Boolean to check if game is networked
+	private bool isMultiplayer;
+	// counter to track game time
+	private float counter = 0;
+
+	// On Awake Menu Selection is disabled
+	void Awake()
+	{
+		SelectionCanvas.enabled = false;
+	}
+
+	// update counter and handle splash screen
+	void Update()
+	{
+		counter += Time.deltaTime;
+		if (counter >= 2) 
+		{
+			SplashScreen.enabled = false;
+		}
+	}
+
+	// Select Multiplayer Mode
+	public void MultiplayerSelectionOn()
+	{
+		SelectionCanvas.enabled = true;
+		ModeCanvas.enabled = false;
+		isMultiplayer = true;
+	}
+
+	// Select Solo Mode
+	public void SoloSelectionOn()
+	{
+		SelectionCanvas.enabled = true;
+		ModeCanvas.enabled = false;
+		isMultiplayer = false;
+	}
+
+	// Start a game, determine if mulitplayer or solo
+	public void LoadOn()
+	{
+		if (isMultiplayer) 
+		{
+			SceneManager.LoadScene (2);
+		}
+		else if (!isMultiplayer) 
+		{
+			SceneManager.LoadScene (1);
+		}
+	}
+
+}
